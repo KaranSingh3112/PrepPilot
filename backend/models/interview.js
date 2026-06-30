7425ab4614ba563939f7f12f62868b7a8e06b76b
KaranSingh3112 <karansingh311206@gmail.com>
2026-06-30 19:44:23 +0530

    Created interview model

---
+++ backend/models/interview.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+
+const questionSchema = newSchema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        default: ""
+    },
+    score: {
+        type: Number,
+        default: null
+    },
+    feedback: {
+        type: String,
+        default: ''
+    },
+},{ _id: false })
+
+const interviewSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true 
+    },
+    jobRole: {
+        type: String,
+        required: true
+    },
+    skilss: [
+        {type: String}
+    ],
+    resumeName: {type: String},
+    qaList: { type: [questionSchema], default: [] },
+    totalScore: {
+        type: Number,
+        default: null
+    },
+    recomendation: {
+        type: String,
+        enum: ['Strong Hire', 'Hire', 'Maybe', 'No Hire', null],
+        default: null
+    },
+    strengths: [{type: String}],
+    weakness: [{type: String}],
+    suggestions: [{type: String}],
+    detailesFeedback: {
+        type: String,
+        default: ''
+    },
+    completed: { type: Boolean, default: false }
+},{timestamps: true})
+
+// Index user for faster history queries
+interviewSchema.index({ user: 1, createdAt: -1 });
+
+module.exports = mongoose.model("Interview", interviewSchema);
