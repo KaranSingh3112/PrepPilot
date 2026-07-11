af04e5ea89d46eadeeb508bf635860b2b3af7398
KaranSingh3112 <karansingh311206@gmail.com>
2026-07-11 18:55:25 +0530

    Implemented basics setups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   return(
     <>
-      <h1>Welcome to PrepPilot!</h1>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <nav>
+        Home Page
+      </nav>
+    </div>
+  )
+}
