7285739ad45580dc0d50eb0d8c2718e265641c06
KaranSingh3112 <karansingh311206@gmail.com>
2026-07-13 12:02:38 +0530

    implemented api base url and interceptors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -15,3 +15,17 @@ api.interceptors.request.use(
     },
     (error) => Promise.reject(error)
 )
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if(error.response?.status === 401){
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            window.location.href = "/login"
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default api;
